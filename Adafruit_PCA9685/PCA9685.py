2fb762283df3c77313acd229e824762b2800c454
Daniel Casner <daniel@anki.com>
2016-10-03 16:14:32 -0700

    Adjusting documentation for fork.

--- Adafruit_PCA9685/PCA9685.py
+++ Adafruit_PCA9685/PCA9685.py
@@ -18,6 +18,11 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+#
+###############################################################################
+# Library forked and reworked to work with the pigpio library.
+# Author: Daniel Casner <www.danielcasner.org>
+#
 from __future__ import division
 import logging
 import time
